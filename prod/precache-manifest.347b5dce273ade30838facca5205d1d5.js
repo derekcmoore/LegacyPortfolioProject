8e71a24ce9f4d875066c9ac839095c29659da990
dmoore <dmoore@dontpaniclabs.com>
2019-08-14 19:26:27 -0500

    Added Firebase hosting

---
+++ prod/precache-manifest.347b5dce273ade30838facca5205d1d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75f05c97ad5b7e6c277cce4715fe084",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb866d732aad340cd614",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0365e4dee5e183f46fb4",
+    "url": "/static/css/main.e6167aec.chunk.css"
+  },
+  {
+    "revision": "eb866d732aad340cd614",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "0365e4dee5e183f46fb4",
+    "url": "/static/js/main.5f8f7129.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
