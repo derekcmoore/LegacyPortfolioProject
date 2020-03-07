ed62e5963f53180e2364a5688878a0664253a687
dmoore28 <moorder1112@gmail.com>
2020-03-06 20:35:48 -0600

    Added navbar scroll functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,73 @@
 import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
-import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Skills from "./components/About/About";
 import Projects from "./components/About/About";
 import Education from "./components/About/About";
 import Contact from "./components/About/About";
 import "./App.scss";
+import $ from "jquery";
 
 class App extends Component {
-  render() {
-    return (
-      <div id="root">
-        <Navigation />
-        <Home />
-        <About />
-        <Skills />
-        <Projects />
-        <Education />
-        <Contact />
-      </div>
-    );
-  }
+    getScrollLocation() {
+        $(document).ready(function() {
+            var sections = {};
+
+            $(".section").each(function() {
+                var hash = $(this).data("hash"),
+                    topOffset = $(this).offset().top;
+                sections[topOffset] = hash;
+            });
+
+            $(window).scroll(function(e) {
+                var scrollTop = $(window).scrollTop();
+                setHash(scrollTop);
+            });
+
+            function setHash(st) {
+                var hash = "";
+                for (var section in sections) {
+                    if (section < st + $(window).height() / 2)
+                        hash = sections[section];
+                }
+                if ("#" + hash !== window.location.hash) {
+                    window.location.hash = hash;
+                }
+            }
+        });
+    }
+
+    render() {
+        this.getScrollLocation();
+        return (
+            <div id="root">
+                <Navigation />
+                <section id="#about" className="section" data-hash="about">
+                    <About />
+                </section>
+                <section id="#skills" className="section" data-hash="skills">
+                    <Skills />
+                </section>
+                <section
+                    id="#projects"
+                    className="section"
+                    data-hash="projects"
+                >
+                    <Projects />
+                </section>
+                <section
+                    id="#education"
+                    className="section"
+                    data-hash="education"
+                >
+                    <Education />
+                </section>
+                <section id="#contact" className="section" data-hash="contact">
+                    <Contact />
+                </section>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,55 +2,84 @@ import React, { Component } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import "./Navigation.scss";
 import {
-  FaUserAlt,
-  FaFolder,
-  FaRegChartBar,
-  FaGraduationCap,
-  FaFileAlt,
-  FaEnvelope
+    FaUserAlt,
+    FaFolder,
+    FaRegChartBar,
+    FaGraduationCap,
+    FaFileAlt,
+    FaEnvelope
 } from "react-icons/fa";
 export default class Navigation extends Component {
-  render() {
-    return (
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        className="flex-column minimized-nav"
-      >
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto flex-column side-nav">
-            <div className="nav-title" href="#home">
-              <h2 className="ml-4 mt-3">Derek</h2>
-              <h2 className="ml-4 mt-1">Moore</h2>
-            </div>
-            <Nav.Link className="nav-item" href="#about">
-              <FaUserAlt className="mr-4 ml-2 nav-user-icon" />
-              About
-            </Nav.Link>
-            <Nav.Link className="nav-item" href="#skills">
-              <FaRegChartBar className="mr-4 ml-2 nav-skill-icon" />
-              Skills
-            </Nav.Link>
-            <Nav.Link className="nav-item" href="#projects">
-              <FaFolder className="mr-4 ml-2 nav-project-icon" />
-              Projects
-            </Nav.Link>
-            <Nav.Link className="nav-item" href="#education">
-              <FaGraduationCap className="mr-4 ml-2 nav-school-icon" />
-              Education
-            </Nav.Link>
-            <Nav.Link className="nav-item" href="#contact">
-              <FaEnvelope className="mr-4 ml-2 nav-contact-icon" />
-              Contact
-            </Nav.Link>
-            <Nav.Link className="nav-item" href="#resume">
-              <FaFileAlt className="mr-4 ml-2 nav-resume-icon" />
-              Resume
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            hash: window.location.hash
+        };
+    }
+
+    componentDidMount() {
+        window.addEventListener("hashchange", this.changeState, false);
+    }
+
+    changeState = () => {
+        this.setState({ hash: window.location.hash });
+        var elmnt = document.getElementById(window.location.hash);
+        elmnt.scrollIntoView();
+    };
+
+    componentWillUnmount() {
+        // Make sure to remove the DOM listener when the component is unmounted.
+        window.removeEventListener("hashchange", this.changeState);
+    }
+
+    render() {
+        return (
+            <Navbar
+                collapseOnSelect
+                expand="lg"
+                className="flex-column minimized-nav"
+            >
+                <h2 className="center-title">Derek Moore</h2>
+                <Navbar.Toggle
+                    className="test"
+                    aria-controls="responsive-navbar-nav"
+                />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav
+                        className="mr-auto flex-column side-nav"
+                        activeKey={this.state.hash}
+                    >
+                        <div className="nav-title">
+                            <h2 className="ml-4 mt-3">Derek</h2>
+                            <h2 className="ml-4 mt-1">Moore</h2>
+                        </div>
+                        <Nav.Link className="nav-item" href="#about">
+                            <FaUserAlt className="mr-4 ml-2 nav-user-icon" />
+                            About
+                        </Nav.Link>
+                        <Nav.Link className="nav-item" href="#skills">
+                            <FaRegChartBar className="mr-4 ml-2 nav-skill-icon" />
+                            Skills
+                        </Nav.Link>
+                        <Nav.Link className="nav-item" href="#projects">
+                            <FaFolder className="mr-4 ml-2 nav-project-icon" />
+                            Projects
+                        </Nav.Link>
+                        <Nav.Link className="nav-item" href="#education">
+                            <FaGraduationCap className="mr-4 ml-2 nav-school-icon" />
+                            Education
+                        </Nav.Link>
+                        <Nav.Link className="nav-item" href="#contact">
+                            <FaEnvelope className="mr-4 ml-2 nav-contact-icon" />
+                            Contact
+                        </Nav.Link>
+                        <Nav.Link className="nav-item">
+                            <FaFileAlt className="mr-4 ml-2 nav-resume-icon" />
+                            Resume
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
 }
